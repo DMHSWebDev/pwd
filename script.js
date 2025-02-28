bb4b52f107fa8848c9570ec99585930fcc058c24
DMHSWebDev <donohuekt@worcesterschools.net>
2025-02-28 05:21:09 -0500

    Update

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll('.button-30:not(.home-button)');
+    buttons.forEach((button, index) => {
+        setTimeout(() => {
+            if (index % 2 === 0) {
+                button.classList.add('slide-in-left');
+            } else {
+                button.classList.add('slide-in-right');
+            }
+        }, index * 200); // Stagger the animations by 200ms
+    });
+});
