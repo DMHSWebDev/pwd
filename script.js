68b68c31d0a3ca95cef006e3eafaeb3df6a76e0b
DMHSWebDev <donohuekt@worcesterschools.net>
2025-03-14 05:27:57 -0400

    Update

--- script.js
+++ script.js
@@ -10,4 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }, index * 200); // Stagger the animations by 200ms
     });
+
+
+    function updateClock() {
+        const clock = document.getElementById('clock');
+        const now = new Date();
+        let hours = now.getHours();
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        hours = hours < 10 ? hours : String(hours).padStart(2, '0');
+        clock.textContent = `${hours}:${minutes}:${seconds}`;
+    }
+    setInterval(updateClock, 1000);
+    updateClock(); // Initial call to display the clock immediately
+
+    let menu_toggle = document.querySelector('.mobile-toggle');
+    let menu = document.querySelector('nav ul');
+    menu_toggle.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
 });
